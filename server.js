4113166fc539bd6467f3a3a6887fb0353efd90e6
riddhijain25 <riddhijain25801@gmail.com>
2026-09-10 11:10:03 +0530

    server.js added PORT as a const for cloudrun deployment

--- server.js
+++ server.js
@@ -460,4 +460,8 @@ app.get('/get-current-user', verifyFirebaseToken, async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
